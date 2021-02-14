a31da5b96268bc8db52ac017254fc82007f717fd
Bill <billxs@hotmail.com>
2021-02-14 17:57:22 +0200

    Added parser for UPDATE query

--- miniParser.py
+++ miniParser.py
@@ -3,12 +3,17 @@ from pyparsing import *
 from database import Database
 from smallRelationsInsertFile import db
 
+
 selectStmt = Forward()
+updateStmt = Forward()
+insertStmt = Forward()
+deleteStmt = Forward()
+createStmt = Forward()
 
 keywords = {
     k: CaselessKeyword(k)
     for k in """\
-    SELECT FROM WHERE
+    SELECT FROM WHERE UPDATE SET
     """.split()
     }
 vars().update(keywords)
@@ -16,37 +21,109 @@ vars().update(keywords)
 
 #SELECT, FROM, WHERE = map(CaselessKeyword, "select from where".split())
 
-ident = Word(alphas,alphanums + "_$").setName("identifier")
+ident = Word(alphas, alphanums + "_$").setName("identifier")
 columnName = delimitedList(ident, ".", combine=True).setName("column name")
 columnNameList = Group(delimitedList(columnName))
 tableName = delimitedList(ident, ".", combine=True).setName("table name")
 tableNameList = Group(delimitedList(tableName))
 
 binop = oneOf("< <= == >= >")
-realNum = pyparsing_common .real()
-intNum = pyparsing_common .signed_integer()
+realNum = pyparsing_common.real()
+intNum = pyparsing_common.signed_integer()
 
+#stringVal = Word(alphanums)
+
+#for select
 columnRval = (
-    realNum | intNum | columnName
+    realNum | intNum | columnName | QuotedString('"')
     )
+
+#for update
+setColumnVal = (
+    realNum | intNum | QuotedString('"')
+    )
+
+#for select
 whereCondition = Group(columnName + binop + columnRval)
 
-whereExpression = infixNotation(whereCondition, [
-    ("==",1,opAssoc.RIGHT)],)
+#for update
+whereConditionSet = Group(ident + binop + setColumnVal)
+
+#whereExpression = infixNotation(whereCondition, [("==",1,opAssoc.RIGHT)],)
+
+setCondition = Group(ident + "=" + setColumnVal)
 
 selectStmt <<= (
     SELECT 
     + ("*" | columnNameList)("columns")
     + FROM
     + tableNameList("tables")
-    + Optional(Group(WHERE + whereExpression), "")("where")
+    + Optional(Group(WHERE + whereCondition), "")("where")
     )
 
+#UPDATE Customers SET name = 'Alfred' WHERE ID == 1;
+updateStmt <<= (UPDATE 
+                + ident("table")
+                + SET 
+                + setCondition("column") 
+                + (WHERE + whereConditionSet)("where")
+                )
+
+queries = {
+    "SELECT" : selectStmt,
+    "UPDATE": updateStmt,
+    "INSERT": insertStmt,
+    "DELETE": deleteStmt,
+    "CREATE": createStmt}
+
 simpleSQL = selectStmt
+miniSQL = selectStmt
+
+updateSQL = updateStmt
+selectSQL = selectStmt
 
 if __name__ == "__main__":
-       simpleSQL.runTests(
+    
+    updateTest = "UPDATE A"
+    selectTest = "SELECT * from XYZYY, ABC"
+    try:
+        miniSQL = queries[updateTest.split()[0]]
+        print(miniSQL.parseString(updateTest))
+    
+        miniSQL = queries[selectTest.split()[0]]
+        print(miniSQL.parseString(selectTest))
+    except:
+        print("Fail")
+        
+    try:
+        miniSQL = queries["choco"]
+        print(miniSQL.parseString(selectTest))
+    except KeyError:
+        print("Invalids Start Of Statement")
+        
+    updateSQL.runTests(
         """\
+        
+        #update normal statement
+        UPDATE student SET name="Zhang@" where ID == 1
+        
+        #fail not in string quotes
+        UPDATE student SET name=Zhang where ID == 1
+        
+        #fail not in string quotes
+        UPDATE student SET name="Zhang" where ID == coffee
+        
+        #fail not in string quotes
+        UPDATE student SET name="Zhang" where ID == "1"
+        
+        """
+        )
+        
+    selectSQL.runTests(
+        """\
+        
+        #update
+        UPDATE student SET name="Zhang@" where ID == 1
 
         # multiple tables
         SELECT * from XYZZY, ABC
